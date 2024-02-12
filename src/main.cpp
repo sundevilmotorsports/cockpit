b900011d541a1d23b7585c5ed6e2dbc11430aa5b
Joshua Tenorio <joshliam09@gmail.com>
2024-02-11 18:20:47 -0700

    ad7797 testing

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,12 @@
 #define BRAKE_LOAD_CS_PIN 10
 
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
-
+SPISettings settings(1000000, MSBFIRST, SPI_MODE3);
 void setup() {
-  pinMode(13, OUTPUT);
+  //pinMode(13, OUTPUT);
 
   SPI.begin();
+  Serial.begin(9600);
 
   pinMode(THERMOCOUPLE_CS_PIN, OUTPUT);
   pinMode(BRAKE_LOAD_CS_PIN, OUTPUT);
@@ -25,7 +26,26 @@ void setup() {
 
 	//Reject all incoming messages
 	Can0.setMBFilter(REJECT_ALL);
+
+  digitalWrite(THERMOCOUPLE_CS_PIN, HIGH);
+  digitalWrite(BRAKE_LOAD_CS_PIN, HIGH);
 }
 
 void loop() {
+  Serial.println("hehe");
+
+  uint8_t comm_packet = (1 << 6) | (((4) & 0x7) << 3);
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE3));
+  digitalWrite(BRAKE_LOAD_CS_PIN, LOW);
+
+  SPI.transfer(comm_packet);
+
+  uint8_t result = SPI.transfer(0x00);
+
+  digitalWrite(BRAKE_LOAD_CS_PIN, HIGH);
+  SPI.endTransaction();
+
+
+  Serial.println(result);
+  delay(1000);
 }
